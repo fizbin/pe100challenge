dd6d2cbd076797704bfde5aef6b6a5485c2dcd1a
Daniel Martin <fizbin@gmail.com>
2021-04-19 16:10:53 -0400

    Faster problem 56 (requires rust 1.51)

--- problem56/src/main.rs
+++ problem56/src/main.rs
@@ -1,7 +1,9 @@
 use num_bigint::BigUint;
 use num_traits::Zero;
 use std::cmp::max;
+use std::convert::From;
 use std::convert::TryFrom;
+use std::iter::Iterator;
 
 fn main() {
     let mut summax = 0;
@@ -15,11 +17,19 @@ fn main() {
 
 fn powsum(a: u32, b: u32) -> u32 {
     let mut full = BigUint::from(a).pow(b);
-    let mut digsum = BigUint::zero();
+    let mut digsum = 0u32;
     while !full.is_zero() {
-        digsum += full.clone() % 10u32;
+        let dig: u32 = full
+            .iter_u32_digits()
+            .reduce(|acc, d| acc % 10 + 6 * (d % 10))
+            .unwrap()
+            % 10;
+        digsum += dig;
+        // Slower ways to get the last digit:
+        //  u32::from(full.to_radix_le(10)[0]);
+        //  full.clone() % 10u32;
+        //  &full % 10u32;
         full /= 10u32;
     }
     u32::try_from(digsum).unwrap()
 }
-
